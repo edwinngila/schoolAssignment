17974f9d5bf763c33a05769b6a977ec25eee9aba
Edwin ngila kyalo <edwinngilakyalo@gmail.com>
2023-10-02 09:10:36 +0300

    seccond commit

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
   
 Route::get('/home', [HomeController::class, 'index'])->name('home');
   
